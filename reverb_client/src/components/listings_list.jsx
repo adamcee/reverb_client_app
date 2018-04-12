15f2f97a2ea9155b26e7dc8ec906891c5c5580b0
adamcee <adam.cahan@gmail.com>
2018-04-12 14:32:43 -0500

    - show thumbnails of listings. 	modified:   components/listings_list.jsx

--- reverb_client/src/components/listings_list.jsx
+++ reverb_client/src/components/listings_list.jsx
@@ -6,10 +6,14 @@ function ListingsList(props) {
     if(!listings.length) {
         return null;
     }
-    const renderedListings = listings.map((l, i) => <li key={i}>{l.title}</li>);
+    const renderedListings = listings.map((l, i) => {
+        const href = l.photos[0]._links.thumbnail.href;
+        return <li key={i}>{l.title}<img src={href}/></li>
+    });
     return (
         <ul>
             {renderedListings}
+
         </ul>
     );
 }
